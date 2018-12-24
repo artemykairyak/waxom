79e2381b5d4295675c1249b1655060e982ca9d13
Artemy Kairyak <artemykairyak@yandex.com>
2018-12-24 19:41:43 +0400

    added header slider

--- build/js/script.js
+++ build/js/script.js
@@ -1,105 +1,175 @@
-var card = document.querySelectorAll('.portfolio__cards_card');
-var portfolioButtons = document.querySelectorAll('.portfolio__buttons a');
-var playButton = document.querySelector('.video-presentation__play-btn');
-var posts = document.querySelectorAll('.post');
-var recentPosts = document.querySelectorAll('.recent-post');
-var twitterPosts = document.querySelectorAll('.twitter-post');
-var bottomMenuItems = document.querySelectorAll('.footer__bottom li');
+window.onload = function() {
 
-for (var i = 0; i< twitterPosts.length; i++) {
-	twitterPosts[i].addEventListener('mouseenter', onTwitterPostsEnter);
-	twitterPosts[i].addEventListener('mouseleave', onTwitterPostsLeave);
-}
+	$(document).ready(function(){
+	  $('.header-slider').slick({
+	  	prevArrow: $('#arrow-left'),
+		nextArrow: $('#arrow-right'),
+		speed: 500,
+		infinite: false,
+		dots: true,
+		appendDots: $('.slider'),
+		customPaging: function(slider, i) {
+      		return '<span class="dot"></span>';
+    	},
+	  })
 
-for (var i = 0; i< bottomMenuItems.length; i++) {
-	bottomMenuItems[i].addEventListener('mouseenter', onbottomMenuItemsEnter);
-	bottomMenuItems[i].addEventListener('mouseleave', onbottomMenuItemsLeave);
-}
+	  $('.header-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  			var currentDot = $(sliderDots[currentSlide]);
+  			for (var i = 0; i < sliderDots.length; i++) {
+  				if (i === currentSlide) {
+  					currentDot.addClass('active-dot');
+  				} else {
+  					sliderDots[i].classList.remove('active-dot');
+  				}
 
-for (var i = 0; i< recentPosts.length; i++) {
-	recentPosts[i].addEventListener('mouseenter', onRecentPostEnter);
-	recentPosts[i].addEventListener('mouseleave', onRecentPostLeave);
-}
+  				if (currentSlide === 0) {
+  					$('#arrow-left').addClass('disabled-arrow');
+  				} else {
+  					$('#arrow-left').removeClass('disabled-arrow');
+  				}
+  				// КОСТЫЛЬ ПОФИКСИТЬ
+  				if (currentSlide === 4) {
+  					$('#arrow-right').addClass('disabled-arrow');
+  				} else {
+  					$('#arrow-right').removeClass('disabled-arrow');
+  				}
+  			}
+		});
+	});
 
-for (i = 0; i < card.length; i++) {
-	card[i].addEventListener('mouseenter', onCardEnter);
-	card[i].addEventListener('mouseleave', onCardLeave);
-}
+	var card = document.querySelectorAll('.portfolio__cards_card');
+	var portfolioButtons = document.querySelectorAll('.portfolio__buttons a');
+	var playButton = document.querySelector('.video-presentation__play-btn');
+	var posts = document.querySelectorAll('.post');
+	var recentPosts = document.querySelectorAll('.recent-post');
+	var twitterPosts = document.querySelectorAll('.twitter-post');
+	var bottomMenuItems = document.querySelectorAll('.footer__bottom li');
+	var sliderDots = document.querySelectorAll('.slider .dot');
+	var sliderArrows = document.querySelectorAll('.slider-arrow');
+	var sliderArrowLeft = document.querySelector('#arrow-left');
+	var sliderArrowRight = document.querySelector('#arrow-right');
 
-for (i = 0; i < portfolioButtons.length; i++) {
-	portfolioButtons[i].addEventListener('click', onPortfolioButtonClick);
-}
+	// for (var i = 0; i < sliderArrows.length; i++) {
+	// 	sliderArrows[i].addEventListener('click', disableSlideArrows);
+	// }
 
-for (i = 0; i < posts.length; i++) {
-	posts[i].addEventListener('mouseenter', onPostEnter);
-	posts[i].addEventListener('mouseleave', onPostLeave);
-}
+	//sliderArrowLeft.addEventListener('click', onSliderArrowLeftClick);
 
+	// function disableSlideArrows() {
+	// 	var currentArrow;
+	// 	for (var i = 0; i < sliderArrows.length; i++) {
+	// 		currentArrow = $(sliderArrows[i]);
+	// 		if (currentArrow.hasClass('slick-disabled')) {
+	// 			currentArrow.addClass('disabled-arrow');
+	// 		} else {
+	// 			currentArrow.removeClass('disabled-arrow');
+	// 		}
+	// 	}
+	// }
 
-function onPortfolioButtonClick(e) {
-	e.preventDefault();
-	for (i = 0; i < portfolioButtons.length; i++) {
-		if (portfolioButtons[i] === event.target) {
-			portfolioButtons[i].classList.add('selected-portfolio-btn');
-			}	else {
-				portfolioButtons[i].classList.remove('selected-portfolio-btn');
-			}
+	for (var i = 0; i < sliderDots.length; i++) {
+		sliderDots[i].addEventListener('click', onSlideDotsClick);
+		sliderDots[0].classList.add('active-dot');
 	}
-}
 
+	for (var i = 0; i< twitterPosts.length; i++) {
+		twitterPosts[i].addEventListener('mouseenter', onTwitterPostsEnter);
+		twitterPosts[i].addEventListener('mouseleave', onTwitterPostsLeave);
+	}
 
-function onCardEnter(e) {
-	event.target.classList.add('selected-portfolio-card');
-	this.querySelector('.toplabel').style.color = '#ffffff';
-	this.querySelector('.bottomlabel').style.color = '#c7b299';
-	this.querySelector('img').style.filter = 'brightness(50%)';
-	this.querySelector('.arrow').style.opacity = '0';
-} 
+	for (var i = 0; i< bottomMenuItems.length; i++) {
+		bottomMenuItems[i].addEventListener('mouseenter', onbottomMenuItemsEnter);
+		bottomMenuItems[i].addEventListener('mouseleave', onbottomMenuItemsLeave);
+	}
 
-function onCardLeave(e) {
-	event.target.classList.remove('selected-portfolio-card');
-	this.querySelector('.toplabel').style.color = '';
-	this.querySelector('.bottomlabel').style.color = '';
-	this.querySelector('img').style.filter = '';
-	this.querySelector('.arrow').style.opacity = '';
-} 
+	for (var i = 0; i< recentPosts.length; i++) {
+		recentPosts[i].addEventListener('mouseenter', onRecentPostEnter);
+		recentPosts[i].addEventListener('mouseleave', onRecentPostLeave);
+	}
 
-playButton.addEventListener('mouseenter', function(e) {
-	event.target.querySelector('.play-btn-circle').style.transform = 'rotate(360deg)';
-});
+	for (i = 0; i < card.length; i++) {
+		card[i].addEventListener('mouseenter', onCardEnter);
+		card[i].addEventListener('mouseleave', onCardLeave);
+	}
 
-playButton.addEventListener('mouseleave', function(e) {
-	event.target.querySelector('.play-btn-circle').style.transform = '';
-})
+	for (i = 0; i < portfolioButtons.length; i++) {
+		portfolioButtons[i].addEventListener('click', onPortfolioButtonClick);
+	}
 
-function onPostEnter(e) {
-	event.target.classList.add('selected-post');
-};
+	for (i = 0; i < posts.length; i++) {
+		posts[i].addEventListener('mouseenter', onPostEnter);
+		posts[i].addEventListener('mouseleave', onPostLeave);
+	}
 
-function onPostLeave(e) {
-	event.target.classList.remove('selected-post');
-} 
+	function onPortfolioButtonClick(e) {
+		e.preventDefault();
+		for (i = 0; i < portfolioButtons.length; i++) {
+			if (portfolioButtons[i] === event.target) {
+				portfolioButtons[i].classList.add('selected-portfolio-btn');
+				}	else {
+					portfolioButtons[i].classList.remove('selected-portfolio-btn');
+				}
+		}
+	}
 
-function onRecentPostEnter(e) {
-	event.target.classList.add('selected-recent-post');
-}
+	function onCardEnter(e) {
+		event.target.classList.add('selected-portfolio-card');
+		this.querySelector('.toplabel').style.color = '#ffffff';
+		this.querySelector('.bottomlabel').style.color = '#c7b299';
+		this.querySelector('img').style.filter = 'brightness(50%)';
+		this.querySelector('.arrow').style.opacity = '0';
+	} 
 
-function onRecentPostLeave(e) {
-	event.target.classList.remove('selected-recent-post');
-}
+	function onCardLeave(e) {
+		event.target.classList.remove('selected-portfolio-card');
+		this.querySelector('.toplabel').style.color = '';
+		this.querySelector('.bottomlabel').style.color = '';
+		this.querySelector('img').style.filter = '';
+		this.querySelector('.arrow').style.opacity = '';
+	} 
 
-function onbottomMenuItemsEnter(e) {
-	event.target.classList.add('selected-bottom-menu-item');
-}
+	playButton.addEventListener('mouseenter', function(e) {
+		event.target.querySelector('.play-btn-circle').style.transform = 'rotate(360deg)';
+	});
 
-function onbottomMenuItemsLeave(e) {
-	event.target.classList.remove('selected-bottom-menu-item');
-}
+	playButton.addEventListener('mouseleave', function(e) {
+		event.target.querySelector('.play-btn-circle').style.transform = '';
+	})
 
-function onTwitterPostsEnter(e) {
-	event.target.classList.add('selected-twitter-post');
-}
+	function onPostEnter(e) {
+		event.target.classList.add('selected-post');
+	};
 
-function onTwitterPostsLeave(e) {
-	event.target.classList.remove('selected-twitter-post');
+	function onPostLeave(e) {
+		event.target.classList.remove('selected-post');
+	} 
+
+	function onRecentPostEnter(e) {
+		event.target.classList.add('selected-recent-post');
+	}
+
+	function onRecentPostLeave(e) {
+		event.target.classList.remove('selected-recent-post');
+	}
+
+	function onbottomMenuItemsEnter(e) {
+		event.target.classList.add('selected-bottom-menu-item');
+	}
+
+	function onbottomMenuItemsLeave(e) {
+		event.target.classList.remove('selected-bottom-menu-item');
+	}
+
+	function onTwitterPostsEnter(e) {
+		event.target.classList.add('selected-twitter-post');
+	}
+
+	function onTwitterPostsLeave(e) {
+		event.target.classList.remove('selected-twitter-post');
+	}
+
+	function onSlideDotsClick(e) {
+		$(this).parent().parent().find('.dot').removeClass('active-dot');
+		$(this).addClass('active-dot');
+	}
 }
